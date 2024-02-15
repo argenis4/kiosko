b704cc7805c0fc35bbafe8e20b780edfa5a16eec
argenis4 <sistemas@drogueriasur.com.ar>
2024-02-15 16:57:22 -0300

    second commit

---
+++ react-quiosco/src/context/QuioscoProvider.jsx
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+const QuioscoContext = createContext();
+const QuioscoProvider = ({ children }) => {
+    return (
+        <QuioscoContext.Provider value={{}}>
+            {children}
+        </QuioscoContext.Provider>
+    )
+}
+
+export { 
+    QuioscoProvider 
+    }
+export default QuioscoContext
